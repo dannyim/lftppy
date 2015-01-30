7565c6c91d215378481971463969a561308f19e1
Danny Im <minadyn@gmail.com>
2015-01-30 21:32:23 +0000

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'lftppy',
     packages = ['lftppy'],
-    version = '0.1.1',
+    version = '0.1.3',
     description = 'A wrapper around lftp',
     author = 'Danny Im',
     author_email = 'minadyn@gmail.com',
     url = 'https://github.com/minadyn/lftppy',
-    download_url = 'https://github.com/minadyn/lftppy/tarball/0.1.1',
+    download_url = 'https://github.com/minadyn/lftppy/tarball/0.1.3',
     keywords = ['lftp', 'ftp'],
     zip_safe = True,
     install_requires = [
@@ -18,6 +18,6 @@ setup(
         'pyftpdlib',
     ],
     dependency_links = [
-        'git+ssh://git@github.com/pexpect/pexpect.git@e2ff2f47fc7719ebf4375eec81f996362816bb10#egg=pexpect-3.4'
+        'git+https://git@github.com/pexpect/pexpect.git@e2ff2f47fc7719ebf4375eec81f996362816bb10#egg=pexpect-3.4'
     ]
 )
